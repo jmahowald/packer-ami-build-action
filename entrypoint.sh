85cd8b2c8e7f22be900101b4d6873399fb716c1c
Josh Mahowald <joshua.mahowald@gmail.com>
2019-10-16 22:24:15 -0500

    output image ids so downstream can consume

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 env
 pushd $INPUT_WORKING_DIR
@@ -15,6 +15,7 @@ cat /build-info.json
 # section, but it seems to work so ¯\_(ツ)_/¯ 
 
 
+
 jq --argfile f1 $TEMPLATE_FILE  \
    --argfile f2 /build-info.json \
    -n '$f1 * ($f1."builders"[]? * $f2."builders"[]? | { "builders": [.] } )' \
@@ -25,10 +26,14 @@ jq -s '.[0] * .[1]' /build-info.json packer-build-0.json > packer-build-1.json
 echo "using merged file:"
 cat packer-build-1.json
 
+
 if [ "$INPUT_VALIDATE_ONLY" == "true" ]; then
     packer validate packer-build-1.json
 else
-    packer build packer-build-1.json
+    if [ -n $INPUT_VAR_FILE ] && [ -f $INPUT_VAR_FILE ]; then
+        PACKER_ARGS="$PACKER_ARGS -var-file $INPUT_VAR_FILE"
+    fi
+    packer build $PACKER_ARGS packer-build-1.json
     echo "manifest:"
     cat $INPUT_TEMPLATE-manifest.json
     # Extract the ami from the manifest.  manifest looks like
@@ -37,8 +42,12 @@ else
      | jq -r '.builds[0].artifact_id' \
      | cut -d ":" -f 2)
     
-    
+    # Need a newline from the previous output for some reason
     echo ""
     echo ::set-output name=image_id::$image_id
 
+    if [ -n "$INPUT_IMAGE_ID_FILE" ]; then
+        echo "Updating $INPUT_IMAGE_ID_FILE with image id"
+        /update-image-id.sh $INPUT_IMAGE_ID_FILE $INPUT_IMAGE_ID_VARIABLE_NAME $image_id
+    fi
 fi
